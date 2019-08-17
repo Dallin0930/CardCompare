194b0a44c7415afef734f2c9ccf718a249d550a6
Dallin0930 <2869865192@qq.com>
2019-08-17 22:30:58 +0800

    [first_case]:implement_it

--- src/test/java/CardTest/CardCompareTest.java
+++ src/test/java/CardTest/CardCompareTest.java
@@ -10,11 +10,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CardCompareTest {
-    
+
   @Test
   public void should_return_first_player_when_input_hightCard_type(){
-
-
+      
          List<String> cards1 = Arrays.asList("4D");
          List<String> cards2 = Arrays.asList("6H");
 
