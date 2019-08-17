89de399c1a5a3adc4e5e8689af4147635e12758c
Dallin0930 <2869865192@qq.com>
2019-08-17 22:59:40 +0800

    [second_case]:write_and_run_test

--- src/main/java/CardCompare/Handle.java
+++ src/main/java/CardCompare/Handle.java
@@ -6,6 +6,7 @@ public class Handle{
     public String compare(Player player1, Player player2) {
         List<String> str1 = player1.getCards();
         List<String> str2 = player2.getCards();
+
         if(str1.get(0).charAt(0)>str2.get(0).charAt(0)){
             return player1.getPlayName();
         }else{
--- src/test/java/CardTest/CardCompareTest.java
+++ src/test/java/CardTest/CardCompareTest.java
@@ -12,22 +12,25 @@ import org.junit.Test;
 public class CardCompareTest {
 
   @Test
-  public void should_return_first_player_when_input_hightCard_type(){
-      
+  public void should_return_player2_when_input_4D_6H(){
+
          List<String> cards1 = Arrays.asList("4D");
          List<String> cards2 = Arrays.asList("6H");
-
          Handle handle = new Handle();
-
          Player player1 = new Player("player1",cards1);
          Player  player2 = new Player("player2",cards2);
-
          String result = handle.compare(player1,player2);
-
          Assert.assertEquals("player2",result);
+    }
 
-
-
-
-}
+    @Test
+    public void should_return_player2_when_input_4D_TH(){
+        List<String> cards1 = Arrays.asList("4D");
+        List<String> cards2 = Arrays.asList("TH");
+        Handle handle = new Handle();
+        Player player1 = new Player("player1",cards1);
+        Player  player2 = new Player("player2",cards2);
+        String result = handle.compare(player1,player2);
+        Assert.assertEquals("player2",result);
+    }
 }
