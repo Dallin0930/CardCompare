ce09f7631f12221eed754734359b4d2af79d1ebd
Dallin0930 <2869865192@qq.com>
2019-08-18 12:09:08 +0800

    [sixth_case]:write_and_run_test_then_fail

--- src/test/java/CardTest/CardCompareTest.java
+++ src/test/java/CardTest/CardCompareTest.java
@@ -63,4 +63,14 @@ public class CardCompareTest {
         String result = handle.compare(player1,player2);
         Assert.assertEquals("player1",result);
     }
+    @Test
+    public void should_return_player2_when_input_3H3D5S7C9D_4H4D5S7C8D(){
+        List<String> cards1 = Arrays.asList("3H","3D","5S","7C","9D");
+        List<String> cards2 = Arrays.asList("4H","4D","5S","7C","8D");
+        Handle handle = new Handle();
+        Player player1 = new Player("player1",cards1);
+        Player  player2 = new Player("player2",cards2);
+        String result = handle.compare(player1,player2);
+        Assert.assertEquals("player2",result);
+    }
 }
